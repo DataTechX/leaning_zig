5bd5e05938174df04e5889383e92f81d53ce2e7f
J <69198341+JKTheRipperTH@users.noreply.github.com>
2022-07-09 12:25:49 -0700

    new comment

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,13 @@
-const std = @import("std");
+const std = @import("std"); // @import("std") เทียบเท่ากับ @import "std";
 
 pub fn main() anyerror!void {
     const string = [_]u8{'a', 'b', 'c'}; // ลูปค่าออกมาที่ตัวแปร string
 
-    for (string) |character| {
-        std.log.info("{c}", .{character});
+    for (string) |character| { // ลูปค่าออกมาที่ตัวแปร character
+        std.log.info("{c}", .{character}); // พิมพ์ค่าออกมาที่ตัวแปร character
     }
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "basic test" { // ทดสอบการทำงานของฟังก์ชัน main
+    try std.testing.expectEqual(10, 3 + 7); // ตรวจสอบว่า 10 เท่ากับ 3 + 7
 }
